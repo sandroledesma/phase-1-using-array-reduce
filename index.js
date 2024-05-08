61d21a3b8800c5bfa9e63123627653c4bc84f105
Alessandro <sandroledesma@gmail.com>
2024-05-08 12:16:19 -0400

    update files

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch);
+
+// reduce the array into a single value by taking two arguments (total and batch) and 
+// adding that together until all elements of the array have been reduced to a single value
